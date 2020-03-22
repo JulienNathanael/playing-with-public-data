cbea463c5a3bc63688c2a865d6b11cd14c6597e4
Julien Lagarde <julien.lagarde@metroscope.tech>
2020-03-22 16:46:32 +0100

    :sparkles: New COVID19 graph: day 0 <-> 100deaths

--- COVID19/inspect-covid19.py
+++ COVID19/inspect-covid19.py
@@ -6,13 +6,14 @@ import pandas as pd
 import plotly.graph_objects as go
 
 
+with open('default.env', 'r') as file:
+    var_name = 'path_dir_COVID19_repo'
+    first_line = file.readline().strip()
+    assert var_name + '=' == first_line[:len(var_name + '=')]
+    path_dir_COVID19_repo = first_line[len(var_name + '='):]
 
-# ONLY LINE THAT MUST BE MODIFIED: PATH TO THE COVID-19 FOLDER
-path_dir_COVID19_repo = ''
 
-
-
-def plot_states_situation_evolution(df, states, title='', show=True):
+def plot_states_situation_evolution(df, states, title='', html=False, log_y=False, show=True, filename=None):
     # df indexes are states
     # states must all be in df.index
     # df columns: dates
@@ -31,8 +32,14 @@ def plot_states_situation_evolution(df, states, title='', show=True):
 
     fig.update_layout(title=title)
 
+    if log_y is True:
+        fig.update_layout(yaxis_type='log')
+
     if show is True:
         fig.show()
+
+    if html is True and filename is not None:
+        fig.write_html(file=filename)
 
 
 def prepare_df_states(df):
@@ -50,8 +57,8 @@ if __name__ == '__main__':
     df_deaths = prepare_df_states(pd.read_csv(path_deaths))
     df_recovered = prepare_df_states(pd.read_csv(path_recovered))
 
-    # 10 states with highest confirmed cases
-    states_with_most_confirmed = df_confirmed.iloc[:, -1].sort_values(ascending=False).head(10).index
+    # 10 states with highest number of deaths
+    countries_with_most_deaths = df_deaths.iloc[:, -1].sort_values(ascending=False).head(10).index
 
     # Evolution of morbidity estimators (far from perfect...)
     df_deaths_over_confirmed = df_deaths / df_confirmed
@@ -66,7 +73,7 @@ if __name__ == '__main__':
     df_recovered_daily_increase = df_recovered.diff(axis=1)
 
     # Smoothing daily increase per country - exponentially weighted averages: WARNING with delay
-    beta = 0.9
+    beta = 0.8
     df_confirmed_daily_increase_exp_av = pd.DataFrame(index=df_confirmed_daily_increase.index, columns=df_confirmed_daily_increase.columns)
     df_confirmed_daily_increase_exp_av_b_corr = pd.DataFrame(index=df_confirmed_daily_increase.index, columns=df_confirmed_daily_increase.columns)
 
@@ -78,17 +85,37 @@ if __name__ == '__main__':
             df_confirmed_daily_increase_exp_av.iloc[:, nday] = beta * df_confirmed_daily_increase_exp_av.iloc[:, nday - 1] + (1 - beta) * df_confirmed_daily_increase.iloc[:, nday]
             df_confirmed_daily_increase_exp_av_b_corr.iloc[:, nday] = (1 / (1 - beta ** nday)) * df_confirmed_daily_increase_exp_av.iloc[:, nday]
 
+    # Evolution of number of deaths; for each country, day 0 corresponds to the first day with more than 100 deaths
+    df_deaths_filtered = df_deaths[df_deaths.max(axis=1) > 100]  # Keep only countries with more than 100 deaths
+    df_building_day0_100deaths = pd.DataFrame(index=df_deaths_filtered.index)
+    df_building_day0_100deaths['#days < 100 deaths'] = (df_deaths_filtered < 100).sum(axis=1)
+    df_building_day0_100deaths['#days >= 100 deaths'] = (df_deaths_filtered >= 100).sum(axis=1)
+
+    df_deaths_day0_100deaths = pd.DataFrame(index=df_deaths_filtered.index, columns=list(range(-df_building_day0_100deaths['#days < 100 deaths'].max(), df_building_day0_100deaths['#days >= 100 deaths'].max())))
+    for ind_c, country in enumerate(df_deaths_filtered.index):
+        for ind_d, day in enumerate(list(range(-df_building_day0_100deaths['#days < 100 deaths'][country], df_building_day0_100deaths['#days >= 100 deaths'][country]))):
+            df_deaths_day0_100deaths[day][country] = df_deaths_filtered.iloc[ind_c, ind_d]
+
+    # 10days before 100deaths, nothing to focus on
+    df_deaths_day0_100deaths.drop(columns=list(range(-df_building_day0_100deaths['#days < 100 deaths'].max(), -10)), inplace=True)
+
     # Plots
-    plot_states_situation_evolution(df=df_confirmed, states=states_with_most_confirmed, title='Number of confirmed')
-    plot_states_situation_evolution(df=df_deaths, states=states_with_most_confirmed, title='Number of deaths')
-    plot_states_situation_evolution(df=df_recovered, states=states_with_most_confirmed, title='Number of recovered')
-    plot_states_situation_evolution(df=df_deaths_over_confirmed, states=states_with_most_confirmed, title='Evolution of ratio (deaths / confirmed), only when more than 100 deaths')
-    plot_states_situation_evolution(df=df_deaths_over_closed, states=states_with_most_confirmed, title='Evolution of ratio (deaths / (deaths + recovered)), only when more than 100 deaths')
-    plot_states_situation_evolution(df=df_confirmed_daily_increase, states=states_with_most_confirmed, title='Confirmed per country, daily increase')
-    plot_states_situation_evolution(df=df_deaths_daily_increase, states=states_with_most_confirmed, title='Deaths per country, daily increase')
-    plot_states_situation_evolution(df=df_recovered_daily_increase, states=states_with_most_confirmed, title='Recovered per country, daily increase')
-    plot_states_situation_evolution(df=df_confirmed_daily_increase_exp_av_b_corr, states=states_with_most_confirmed,
+    plot_states_situation_evolution(df=df_confirmed, states=countries_with_most_deaths, title='Number of confirmed')
+    plot_states_situation_evolution(df=df_deaths, states=countries_with_most_deaths, title='Number of deaths')
+    plot_states_situation_evolution(df=df_deaths, states=countries_with_most_deaths, log_y=True, title='Number of deaths (log)')
+    plot_states_situation_evolution(df=df_recovered, states=countries_with_most_deaths, title='Number of recovered')
+    plot_states_situation_evolution(df=df_deaths_over_confirmed, states=countries_with_most_deaths, title='Evolution of ratio (deaths / confirmed), only when more than 100 deaths')
+    plot_states_situation_evolution(df=df_deaths_over_closed, states=countries_with_most_deaths, title='Evolution of ratio (deaths / (deaths + recovered)), only when more than 100 deaths')
+    plot_states_situation_evolution(df=df_confirmed_daily_increase, states=countries_with_most_deaths, title='Confirmed per country, daily increase')
+    plot_states_situation_evolution(df=df_deaths_daily_increase, states=countries_with_most_deaths, title='Deaths per country, daily increase')
+    plot_states_situation_evolution(df=df_recovered_daily_increase, states=countries_with_most_deaths, title='Recovered per country, daily increase')
+    plot_states_situation_evolution(df=df_confirmed_daily_increase_exp_av_b_corr, states=countries_with_most_deaths,
                                     title='Confirmed per country, daily increase, exponentially averaged with bias correction - WARNING: delay as a cost of smoothness')
+
+    countries_with_more_than_100states = [country for country in countries_with_most_deaths if country in df_deaths_day0_100deaths.index]
+    plot_states_situation_evolution(df=df_deaths_day0_100deaths, states=countries_with_more_than_100states, html=True,
+                                    filename='covid19_ndeaths_evolution_day0_100deaths.html', title='Number of deaths, day0: about 100 deaths')
+    plot_states_situation_evolution(df=df_deaths_day0_100deaths, states=countries_with_more_than_100states, log_y=True, title='Number of deaths, day0: about 100 deaths (log)')
 
     # Summary tables
     df_summary = pd.DataFrame(
